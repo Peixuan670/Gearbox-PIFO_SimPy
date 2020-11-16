2d4f26a364176823ea9c75b1d252bb2def026145
Peixuan Gao <peixuan670@outlook.com>
2020-11-16 00:00:41 -0500

    Updated draft of Gearbox

--- gearboxII.py
+++ gearboxII.py
@@ -13,16 +13,72 @@ class GearboxII(HW_sim_object):
 
 
         # Initiate all the levels
+        self.levels = []
+
+        self.fifo_r_in_pipe_matrix = [][]
+        self.fifo_r_out_pipe_matrix = [][]
+        self.fifo_w_in_pipe_matrix = [][]
+        self.fifo_w_out_pipe_matrix = [][]
+
+        self.pifo_r_in_pipe_arr = []
+        self.pifo_r_out_pipe_arr = []
+        self.pifo_w_in_pipe_arr = []
+        self.pifo_w_out_pipe_arr = []
+
+        # initiate level 0
+
+        fifo_r_in_pipe_arr = []
+        fifo_r_out_pipe_arr = []
+        fifo_w_in_pipe_arr = []
+        fifo_w_out_pipe_arr = []
+
+        while (index < self.fifo_num_list[0]):
+            fifo_r_in_pipe_arr.append(simpy.Store(env))
+            fifo_r_out_pipe_arr.append(simpy.Store(env))
+            fifo_w_in_pipe_arr.append(simpy.Store(env))
+            fifo_w_out_pipe_arr.append(simpy.Store(env))
+        
+        self.fifo_r_in_pipe_matrix.append(fifo_r_in_pipe_arr)
+        self.fifo_r_out_pipe_matrix.append(fifo_r_out_pipe_arr)
+        self.fifo_w_in_pipe_matrix.append(fifo_w_in_pipe_arr)
+        self.fifo_w_out_pipe_matrix.append(fifo_w_out_pipe_arr)
+
+        level0 = Base_level(env, period, self.granularity_list[0], self.fifo_size_list[0], self.fifo_r_in_pipe_matrix[0], \
+        self.fifo_r_out_pipe_matrix[0], self.fifo_w_in_pipe_matrix[0], self.fifo_w_out_pipe_matrix[0], \
+        fifo_write_latency=1, fifo_read_latency=1, fifo_check_latency=1, fifo_num=10, initial_vc=0)
+        self.levels.append(level0)
+
+
         index = 1
         while (index < self.level_num):
-            self.fifo_r_in_pipe_arr.append(simpy.Store(env))
-            self.fifo_r_out_pipe_arr.append(simpy.Store(env))
-            self.fifo_w_in_pipe_arr.append(simpy.Store(env))
-            self.fifo_w_out_pipe_arr.append(simpy.Store(env))
-            
-            new_fifo = FIFO(env, period, self.fifo_r_in_pipe_arr[index], self.fifo_r_out_pipe_arr[index], self.fifo_w_in_pipe_arr[index], self.fifo_w_out_pipe_arr[index], maxsize=self.fifo_size, write_latency=1, read_latency=2, init_items=[])
-            self.fifos.append(new_fifo) 
 
+            fifo_r_in_pipe_arr = []
+            fifo_r_out_pipe_arr = []
+            fifo_w_in_pipe_arr = []
+            fifo_w_out_pipe_arr = []
+
+            while (index < self.fifo_num_list[index]):
+                fifo_r_in_pipe_arr.append(simpy.Store(env))
+                fifo_r_out_pipe_arr.append(simpy.Store(env))
+                fifo_w_in_pipe_arr.append(simpy.Store(env))
+                fifo_w_out_pipe_arr.append(simpy.Store(env))
+
+            self.fifo_r_in_pipe_matrix.append(fifo_r_in_pipe_arr)
+            self.fifo_r_out_pipe_matrix.append(fifo_r_out_pipe_arr)
+            self.fifo_w_in_pipe_matrix.append(fifo_w_in_pipe_arr)
+            self.fifo_w_out_pipe_matrix.append(fifo_w_out_pipe_arr)
+
+            self.pifo_r_in_pipe_arr.append(simpy.Store(env))
+            self.pifo_r_out_pipe_arr.append(simpy.Store(env))
+            self.pifo_w_in_pipe_arr.append(simpy.Store(env))
+            self.pifo_w_out_pipe_arr.append(simpy.Store(env))
+
+            cur_level = Level(env, period, self.granularity_list[index], self.fifo_size_list[index], self.pifo_thresh_list[index], \
+            self.pifo_size_list[index], self.fifo_r_in_pipe_matrix[index], self.fifo_r_out_pipe_matrix[index], \
+            self.fifo_w_in_pipe_matrix[index], self.fifo_w_out_pipe_matrix[index],self.pifo_r_in_pipe_arr[index], \
+            self.pifo_r_out_pipe_arr[index], self.pifo_w_in_pipe_arr[index], self.pifo_w_out_pipe_arr[index], \
+            fifo_write_latency=1, fifo_read_latency=1, fifo_check_latency=1, fifo_num=10, pifo_write_latency=1, pifo_read_latency=1, initial_vc=0)
+            
             index = index + 1
     
     def enque(self, pkt):
